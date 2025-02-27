dbb13c0808cb937c8c3faa83c4f148925d90acbc
Rex Fernando <rex.fernando@aptoslabs.com>
2025-02-27 16:58:53 -0500

    remove Alex's old make scripts, modify build_lib.sh to use new build system

--- build_lib.sh
+++ build_lib.sh
@@ -2,20 +2,7 @@
 
 set -e
 
-export CC=/usr/bin/clang
-export CXX=/usr/bin/clang++
-
-if [[ $(uname -s) == "Darwin" ]]; then
-  ./build_gmp.sh macos_arm64
-  mkdir -p build_prover_macos_arm64 
-  cd build_prover_macos_arm64
-  cmake .. -DTARGET_PLATFORM=macos_arm64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../package
-  make -j4 && make install
-else
-  mkdir -p build_prover
-  cd build_prover
-  cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../package ..
-  make -j$(nproc)
-  make install
-fi
+meson setup --native-file=native-env.ini build --wipe
+cd build 
+meson compile
 
